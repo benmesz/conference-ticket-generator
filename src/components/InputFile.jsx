b641df59a98b254d1455a0abee8a9b0cff87764b
Bence Mészáros <bencemeszaros@Gyulas-iMac.local>
2025-07-21 17:54:21 +0300

    File input shows preview of uploaded image. Preview formatted to look like the placeholder container

--- src/components/InputFile.jsx
+++ src/components/InputFile.jsx
@@ -1,13 +1,30 @@
 import styles from "./inputfield.module.css";
+import { useState } from "react";
 
 const InputFile = () => {
+  const [previewUrl, setPreviewUrl] = useState("/icon-upload.svg");
+
+  const handleFileChange = (e) => {
+    const file = e.target.files?.[0];
+    const imgLink = URL.createObjectURL(file);
+    // let imgLink = URL.createObjectURL(e.dataTransfer.files[0]);
+    setPreviewUrl(imgLink);
+    console.log(imgLink);
+  };
+
   return (
     <div>
       <h3>Upload Avatar</h3>
-      <label htmlFor="input-file" id={styles.dropArea}>
-        <input type="file" accept="image/*" id="input-file" hidden />
+      <label htmlFor="input-file" id={styles.dropArea} tabIndex="0">
+        <input
+          type="file"
+          accept="image/*"
+          id="input-file"
+          hidden
+          onChange={handleFileChange}
+        />
         <div id={styles.imgView}>
-          <img src="/icon-upload.svg" />
+          <img src={previewUrl} />
         </div>
         <p>Drag and drop or click here to upload image</p>
       </label>
